a1096576c05812b17f3e19cae00f5178d59968f7
ARVIN MANOJ <58823777+arvinmanoj15@users.noreply.github.com>
2023-10-30 14:25:10 -0400

    Create esp_32_udp_coms.py

---
+++ esp_32_udp_coms.py
@@ -0,0 +1,28 @@
+import socket
+import random
+import time
+
+# Set the ESP32's IP address and port
+esp32_ip = 'ESP32_IP_ADDRESS'  # Replace with your ESP32's IP address
+esp32_port = 12345  # Use the same port on both sides
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+try:
+    while True:
+        # Generate a random value between 0 and 9
+        value = random.randint(0, 9)
+        
+        # Send the value to the ESP32
+        message = str(value)
+        sock.sendto(message.encode(), (esp32_ip, esp32_port))
+        
+        print(f"Sent value: {value}")
+        
+        # Wait for 10 seconds
+        time.sleep(10)
+except KeyboardInterrupt:
+    print("Program terminated.")
+finally:
+    sock.close()
