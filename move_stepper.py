b3f6603b549fbc8bf51a467f48892f73f628a5a4
arvinmanoj15 <arvinmanoj.k@gmail.com>
2023-11-29 20:01:59 -0500

    Stepper Phase1 Working

--- move_stepper.py
+++ move_stepper.py
@@ -21,7 +21,7 @@ halfstep_seq = [
   [1,0,0,1]
 ]
 
-for i in range(512):
+for i in range(3000):
   for halfstep in range(8):
     for pin in range(4):
       GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
